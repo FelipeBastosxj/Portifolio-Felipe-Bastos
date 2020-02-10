d8069069ef2800e026f2904b72df66fa880fe55c
FelipeBastosxj <55720172+FelipeBastosxj@users.noreply.github.com>
2020-02-10 14:18:18 -0300

    Mudança Total-1

---
+++ mudanças.js
@@ -0,0 +1,28 @@
+botoes = document.querySelectorAll('.b1')
+partes = document.querySelectorAll('.s1')
+
+function tabs(index){
+    partes.forEach(e =>{
+        e.classList.remove('ativo')
+    })
+    partes[index].classList.add('ativo')
+}
+
+botoes.forEach((item, posicao)=>{
+    item.addEventListener('click', ()=>{
+        tabs(posicao)
+    })
+})
+
+/* maquina*/
+
+function typeWriter(elemento){
+    const textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i )=>{
+        setTimeout(() => elemento.innerHTML += letra, 75 * i)
+    })
+}
+
+const titulo = document.querySelector('.subtitulo')
+typeWriter(titulo)
